8cbcdb16ed3f761dd6096cc475d076fa7197403f
Bhargavi0576 <90825125+Bhargavi0576@users.noreply.github.com>
2021-10-08 14:36:39 +0530

    main.rs - assessment
    Initially checks the number of arguments and if are valid, proceeds to convert the given input file and parses into Person
    Upon converting given each line of input file into byte stream, it is written to output file

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+extern crate protobuf;
+use std::fs::File;
+use std::io::{self,BufRead};
+use std::io::prelude::*;
+use std::path::Path;
+use std::env;
+pub mod Person_msg;
+
+// To read each line from given input file 
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+  
+// To create a output file by converting text into byte streams 
+fn convert()
+{
+    let mut file=File::create("OUTPUT_FILE").expect("Create Failed");
+    let mut count=0;
+    if let Ok(lines) = read_lines("./Input_file.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                let text=format!("{}\n",ip);
+                let v:Vec<&str>=ip.split(',').collect();
+                let mut person=Person_msg::Person::new();
+                count+=1;
+                println!("Person {} details: ",count);
+                person.set_lname(v[0].to_string());
+                person.set_fname(v[1].to_string());
+                person.set_dob(v[2].to_string());
+                println!("{:?}",person);
+                
+                let bytes=text.into_bytes();
+                let ab=protobuf::Message::write_length_delimited_to_bytes(&person);
+                match file.write_all(&bytes){
+                    Err(why) => panic!("couldn't write: {}",why),
+                    Ok(_) => bytes,
+                };
+                println!("{:?}",ab);
+            }
+        }
+        
+        println!("\nSuccessfully written to the file");
+    }
+}
+
+fn help() {
+    println!("Too Many arguments");
+}
+
+fn getting_arguments() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        // no arguments passed
+        1 => {
+            println!("No arguments Received");
+        },
+        // one argument passed
+        2 => {
+            println!("Received only Input file\n");
+        }
+        // two arguments passed
+        3 =>{
+            println!("Received both the files\n");
+            convert();
+        }
+        // all the other cases
+        _ => {
+            // show a help message
+            help();
+        }
+    }
+}
+
+
+fn main()
+{
+    println!("");
+    getting_arguments();
+}
